281d92c2ffd134c4766c143605a5af19fa160a9d
ricky wahba <rjwahba@gmail.com>
2020-01-16 16:18:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ def summon_captain_planet(calls)
   new
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words
+  
 end
 
 def find_the_cheese# code an argument here
